e4f55bc17c4ecc71937d6183fd0a9eccdd6fb3da
Filippo Floramo <filifloramo@gmail.com>
2022-09-26 23:59:07 +0200

    finished font color skills

--- src/components/Skills/SkillsContent/SkillsContent.tsx
+++ src/components/Skills/SkillsContent/SkillsContent.tsx
@@ -29,15 +29,18 @@ export default function SkillsContent(): JSX.Element {
                   <p className="skills--text">
                      <span></span>
                      <Pause ms={1500} />
+
                      <span className="purple">export default</span> <span className="blue">function</span> <span className="yellow">Skills</span><span className="more-yellow">{`()`}</span> {`{`}
                      <br />
                      &emsp; <span className="purple">return</span> {`(`}
                      <br />
-                     &emsp; <span>{`<>`}</span>
-                     <br />
-                     {textElements}
-                     &emsp; {`</>`}
-                     <br />
+                     <span className="grey">
+                        &emsp; <span>{`<>`}</span>
+                        <br />
+                        {textElements}
+                        &emsp; {`</>`}
+                        <br />
+                     </span>
                      &emsp;{`)`}
                      <br />
                      {`}`}
