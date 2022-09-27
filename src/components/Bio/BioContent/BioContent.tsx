d897fb70b9043e2d31c69cd68202f7a948298d10
Filippo Floramo <filifloramo@gmail.com>
2022-09-27 23:37:18 +0200

    final fix

--- src/components/Bio/BioContent/BioContent.tsx
+++ src/components/Bio/BioContent/BioContent.tsx
@@ -105,7 +105,7 @@ export default function BioContent() {
             animate="animate"
          >
             <p>
-               Yup, that's me, a curious guy who fell in love with coding while studying Digital Marketing on the platform Start2impact.
+               Yup, that's me, a curious guy who fell in love with coding while studying Digital Marketing on the platform <a href="https://www.start2impact.it/" rel="noreferrer" target="_blank" className="s2i">Start2impact</a>.
                Everytime I write some lines of code, I get excited about the fact of creating something from scratch and see it come to life in front of the screen.
                <br />
                With that in mind, I decided to shift my field of study and fully jump into this world of strings and functions.
@@ -136,10 +136,10 @@ export default function BioContent() {
                      On March 2021 I graduate with a bachelor's degree in Business Management at the University of Messina.
                   </li>
                   <li>
-                     On April 2021 I join <a href="https://www.start2impact.it/" rel="noreferrer" target="_blank" className="s2i">Start2impact</a>'s community, choosing the Digital Marketing path.
+                     On April 2021 I join Start2impact's community, choosing the Digital Marketing path.
                   </li>
                   <li>
-                     On January 2022 I switch to web development while studying SEO on the <a href="https://www.start2impact.it" rel="noreferrer" target="_blank" className="s2i">S2i</a> platform. It was love at first sight, and I didn't get back since.
+                     On January 2022 I switch to web development while studying SEO on the S2i platform. It was love at first sight, and I didn't get back since.
                   </li>
                </ul>
             </motion.div>
